7dddf9b6f394c2dc8d685c06f0ae7add416e23bd
Bouarma <laroussiaziz13@gmail.com>
2021-03-28 13:18:49 +0100

    demon skeleton

--- code/ennemy.c
+++ code/ennemy.c
@@ -4,10 +4,14 @@
 
 void animerEnnemiwalking(ennemi *e, SDL_Surface *screen)
 {
+	e->postion_sprite.w=64;
+	e->postion_sprite.h=64;
+	e->direction = 1;
+	printf("///////////////////1\n");
 	if (e->direction == 1)
 	{
 		e->postion_sprite.y=128;
-		switch (*e->postion_sprite.x)
+		switch (e->postion_sprite.x)
 		{
 			case 0:
 			e->postion_sprite.x=512;
@@ -17,6 +21,7 @@ void animerEnnemiwalking(ennemi *e, SDL_Surface *screen)
 			break;
 		}
 	}
+	printf("///////////////////2\n");
 	if (e->direction == 2)
 	{
 		e->postion_sprite.y=192;
@@ -26,18 +31,22 @@ void animerEnnemiwalking(ennemi *e, SDL_Surface *screen)
 			e->postion_sprite.x=0;
 			break;
 			default:
-			*e->postion_sprite.x+=64;
+			e->postion_sprite.x+=64;
 			break;
 		}
 	}
-	 for (int x = 0; x < 500 / 64; x++) {
+	printf("///////////////////3\n");
+	
+
+      SDL_BlitSurface(grass,NULL,screen,NULL);
+	 /*for (int x = 0; x < 500 / 64; x++) {
             for (int y = 0; y < 500 / 64; y++) {
                 SDL_Rect position;
                 position.x = x * 64;
                 position.y = y * 64;
                 SDL_BlitSurface(grass, NULL, screen, &position);
             }
-        }
+        }*/
 	SDL_BlitSurface(ennemy, &(e->postion_sprite), screen, &(e->postion_ecran));
 	SDL_Flip((screen));
 }
--- code/main.c
+++ code/main.c
@@ -6,15 +6,15 @@ int main ()
     ennemi E;
 
     int continuer=1;
-	SDL_Init(SDL_INIT_VIDEO);
-	screen = SDL_SetVideoMode(500, 500, 0, 0);
-	E.postion_ecran.x=50;
-	E.postion_ecran.y=50;
-	temp   = IMG_Load("../Assets/Ennmy_models/Skeleton_Sprite.png");
+    SDL_Init(SDL_INIT_VIDEO);
+    screen = SDL_SetVideoMode(500, 500, 0, 0);
+    E.postion_ecran.x=50;
+    E.postion_ecran.y=50;
+	temp   = IMG_Load("Skeleton_Sprite_sheet.png");
     ennemy = SDL_DisplayFormat(temp);
     SDL_FreeSurface(temp);
-    temp  = SDL_LoadBMP("../Assets/Background_Levels/grass.bmp");
-    background = SDL_DisplayFormat(temp);
+    temp  = IMG_Load("grass_15.png");
+    grass = SDL_DisplayFormat(temp);
     SDL_FreeSurface(temp);
     while (continuer)
     animerEnnemiwalking(&E, screen);
