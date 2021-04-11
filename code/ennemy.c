71338c93ad9bacfcbae6fea752475015011a2ad2
Bouarma <laroussiaziz13@gmail.com>
2021-04-11 11:26:28 +0100

    added bg and changed skel

--- code/ennemy.c
+++ code/ennemy.c
@@ -4,46 +4,86 @@
 
 void animerEnnemiwalking(ennemi *e, SDL_Surface *screen)
 {
+	
 	e->postion_sprite.w=64;
 	e->postion_sprite.h=64;
 	e->direction = 1;
 	e->postion_sprite.y=128;
-	printf("///////////////////1\n");
+	//printf("///////////////////1\n");
 	if (e->direction == 1)
 	{
+	
 		e->postion_sprite.x=e->postion_sprite.x-64;
 		if (e->postion_sprite.x<0)
 		{
+			
 			e->postion_sprite.x=512;	
+		
 		}
+
 	}
-	printf("///////////////////2\n");
+	//printf("///////////////////2\n");
 	if (e->direction == 2)
-	{
+	{   
+		
 		e->postion_sprite.y=192;
 		switch (e->postion_sprite.x)
-		{
+		{   
 			case 512:
 			e->postion_sprite.x=0;
 			break;
 			default:
 			e->postion_sprite.x+=64;
 			break;
+	     
+
 		}
 	}
-	printf("///////////////////3\n");
+	//printf("///////////////////3\n");
 	
+	SDL_Delay(16);
 
       SDL_BlitSurface(grass,NULL,screen,NULL);
-	 /*for (int x = 0; x < 500 / 64; x++) {
-            for (int y = 0; y < 500 / 64; y++) {
+	 for (int x = 0; x < 700 / 64; x++) {
+         
+
+            for (int y = 0; y <500 / 64; y++) 
+            {
+                
                 SDL_Rect position;
                 position.x = x * 64;
                 position.y = y * 64;
+                
                 SDL_BlitSurface(grass, NULL, screen, &position);
-            }
-        }*/
+                      }
+        }
+
 	SDL_BlitSurface(ennemy, &(e->postion_sprite), screen, &(e->postion_ecran));
 	SDL_Flip((screen));
 }
+
+                    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- code/main.c
+++ code/main.c
@@ -1,23 +1,85 @@
 #include "header.h"
 
 
-int main ()
+int main()
 {
     ennemi E;
 
-    int continuer=1;
+    int continuer=1,couleurNoire;
     SDL_Init(SDL_INIT_VIDEO);
-    screen = SDL_SetVideoMode(500, 500, 0, 0);
-    E.postion_ecran.x=50;
-    E.postion_ecran.y=50;
-	temp   = IMG_Load("Skeleton_Sprite_sheet.png");
+
+    screen = SDL_SetVideoMode(700, 500, 0, 0);
+    E.postion_ecran.x=500;
+    E.postion_ecran.y=417;
+	
+    temp   = IMG_Load("Skeleton_Sprite_sheet.png");
     ennemy = SDL_DisplayFormat(temp);
     SDL_FreeSurface(temp);
-    temp  = IMG_Load("grass_15.png");
+    temp  = IMG_Load("background12.png");
     grass = SDL_DisplayFormat(temp);
     SDL_FreeSurface(temp);
+    couleurNoire= SDL_MapRGB(screen-> format, 0, 0, 0);
+    SDL_SetColorKey(ennemy, SDL_SRCCOLORKEY | SDL_RLEACCEL,couleurNoire);
+    SDL_Flip(screen);
+    
     while (continuer)
-    SDL_Delay(50);
+    
     animerEnnemiwalking(&E, screen);
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
